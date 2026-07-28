76100a9ccbd52aa7a8b3b28092f7fe11a84c2da9
tyagiakshay21 <tyagiakshay2106@gmail.com>
2026-07-28 15:33:41 +0530

    hi

--- index.js
+++ index.js
@@ -28,22 +28,121 @@
 // }
 // addWithmsg(add,"Akshay");
 
-function login(msg,error){
-    if(error){
-        console.log("Error :"+error);
-    }
-    else{
-        console.log(msg);
-    }
-}
+// function login(msg,error){
+//     if(error){
+//         console.log("Error :"+error);
+//     }
+//     else{
+//         console.log(msg);
+//     }
+// }
 
-function loginhandler(username,password,clbk){
-    if(username=="Akshay"&&password == "12345"){
-        clbk("success",null);
+// function loginhandler(username,password,clbk){
+//     if(username=="Akshay"&&password == "12345"){
+//         clbk("success",null);
+//     }
+//     else{
+//         clbk(null,"Invalid username or password");
+//     }
+// }
+// loginhandler("Akshay","12345",login);
+// loginhandler("Divyam","12345",login);
+
+//console.log("one");
+//console.log("two");
+// for(i=1;i<100;i++){
+//     console.log("two");
+// }
+// setTimeout(() =>{console.log("two");
+// }, 1000)
+
+// console.log("three");
+
+// setTimeout(()=>{
+//     console.log("one");
+//     setTimeout(()=>{
+//         console.log("two");
+//         setTimeout(()=>{
+//             console.log("three");
+//             setTimeout(()=>{
+//                 console.log("four");
+//                 setTimeout(()=>{
+//                     console.log("five");
+//                 },1000)
+//             },1000)
+//         },1000)
+//     },1000)
+
+// })
+
+
+// const mypromise = new Promise ((resolve,reject)=>{
+//     const username = "Akshay";
+//     const password ="123456";
+//     if(username=="Akshay"&& password =="123456"){
+//         resolve(" Success");
+//     }
+//     else{
+//         reject ("Invalid details.");
+//     }
+// })
+
+// mypromise.then((msg)=>
+// {
+//     console.log(msg);
+// }).catch((msg)=>{
+//     console.log(msg);
+// }).finally(()=>{
+//     console.log("All done");
+// })
+
+// const EvenOdd = new Promise((resolve,reject)=>{
+//     const num = 5;
+//     if(num%2==0){
+//         resolve("Even number");
+//     }
+//     else{
+//         reject("Odd number");
+//     }
+// })
+
+// EvenOdd.then((msg)=>{
+//     console.log(msg);
+// }).catch((msg)=>{
+//     console.log(msg);
+// }).finally(()=>{
+//     console.log("done");
+// })
+
+// async function handleData(){
+//     try {
+//         const result = await mypromise;
+//         console.log(result);
+//     }catch(error){ 
+//         console.log(error);
+//     }finally{
+//         console.log("All done");
+//     }
+// }
+// handleData();
+
+const orderSuccess = new Promise((resolve,reject)=>{
+    if(true){
+        resolve("Order placed sucessfully");
     }
     else{
-        clbk(null,"Invalid username or password");
+        reject("Order failed");
     }
 }
-loginhandler("Akshay","12345",login);
-loginhandler("Divyam","12345",login);
+)
+async function orderHandel(){
+    try{
+        const result = await orderSuccess;
+        console.log(result);
+    }catch(error){
+        console.log(error);
+    }finally{
+        console.log("All done");
+    }
+}
+orderHandel();
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+mypromise.then((msg)=>
+// {
+//     console.log(msg);
+// }).catch((msg)=>{
+//     console.log(msg);
+// }).finally(()=>{
+//     console.log("All done");
+// })
